788f226adf908e70ce0046b899b8a981355da805
Aleksandr <kino_mac@mail.ru>
2022-05-03 19:32:23 +0300

    Доработано решение 34-ой задачи

--- Task34/task34.py
+++ Task34/task34.py
@@ -2,18 +2,32 @@
 
 from random import randint
 
+# Метод вычисления степени многочлена (k) и высшего коэффициента (a). Допустимые значения k >= 1 a >= 0
+
+def coef(str, str_need):                
+    index_s = str.find(str_need)
+    if index_s != -1:
+        if str_need == '*x**':
+            index_f = str.find(' ')
+            k = int(str[index_s+4:index_f])
+        else:
+            index_f = str.find(' ')
+            k = 1
+        a = int(str[0:index_s])
+        str = str[index_f + 3:]
+    else:
+        k = index_s
+    return str, k, a
+
 def receive_index(str1):
-    index_s = str1.find('*x**')
-    index_f = str1.find(' ')
-    k = int(str1[index_s+4:index_f])
-    list1 = [0 for item in range(k+1)]
-    list1[k] = int(str1[:index_s])
-    str1 = str1[index_f + 3:]
-    index_s = str1.find('*x')
-    index_f = str1.find(' ')
-    list1[k-1] = int(str1[:index_s])
-    str1 = str1[index_f + 3:]
-    list1[k-2] = int(str1[:index_s])
+    str1_list = coef(str1, '*x**')
+    list1 = [0 for item in range(str1_list[1]+1)]
+    list1[str1_list[1]] = str1_list[2]
+    str1 = str1_list[0]
+    str1_list = coef(str1, '*x')     # вычисление 1 коэффициента
+    list1[1] = str1_list[2]
+    str1 = str1_list[0]              # вычисление 0 коэффициента
+    list1[0] = int(str1[:3])
     return list1
 
 def mult(list, k):
@@ -56,7 +70,7 @@ txt2.close()
 print(str1)
 print(str2)
 
-list1 = receive_index(str1)  
+list1 = receive_index(str1)
 list2 = receive_index(str2)
 
 print(list1, list2)
